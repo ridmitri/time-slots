5c68e509dc482abf239f7ec03d967904c9b2b627
Dmitry Ryzhov <ridmitri@gmail.com>
2022-06-20 10:03:02 +0200

    cleanup

--- src/utils/transformHours/index.ts
+++ src/utils/transformHours/index.ts
@@ -1,73 +1,51 @@
 import secondsToHours, { beforeNoon } from '../secondsToHours';
 
-type DayOfWeek =
-  | 'monday'
-  | 'tuesday'
-  | 'wednesday'
-  | 'thursday'
-  | 'friday'
-  | 'saturday'
-  | 'sunday';
+const DAYS = [
+  'monday',
+  'tuesday',
+  'wednesday',
+  'thursday',
+  'friday',
+  'saturday',
+  'sunday',
+] as const;
+
+type DayOfWeek = typeof DAYS[number];
 
 type OpeningHours = {
   type: 'open' | 'close';
   value: number;
 };
 
-export type Payload = {
-  [key in DayOfWeek]: OpeningHours[];
-};
+export type Payload = Record<DayOfWeek, OpeningHours[]>;
 
 export type TransformedValue = {
-  type: 'open' | 'close';
   hour: number;
   beforeNoon: boolean;
 };
 
-type TransformedHours = TransformedValue[];
+export type Result = Record<DayOfWeek, TransformedValue[] | null>;
 
-export type Result = {
-  [key in DayOfWeek]?: TransformedHours | null;
-};
-
-const getNextDay = (dayOfWeek: DayOfWeek) => {
-  const days = [
-    'monday',
-    'tuesday',
-    'wednesday',
-    'thursday',
-    'friday',
-    'saturday',
-    'sunday',
-  ] as const;
-
+const getNextDay = (dayOfWeek: DayOfWeek, days = DAYS) => {
   const todayIndex = days.indexOf(dayOfWeek);
-  const nextDay = days[(todayIndex + 1) % 7];
+  const nextDay = days[(todayIndex + 1) % days.length];
 
   return nextDay;
 };
 
-const N_A = -1;
-
-const arrangeOverlappingDays = (payload: Payload, notAssigned = N_A) => {
+const shiftNightHours = (payload: Payload) => {
   const result = {} as Payload;
 
-  Object.entries(payload).forEach(([dayOfWeek, openingHours]) => {
-    const day = dayOfWeek as keyof Payload;
-    result[day] = payload[day];
+  Object.entries(payload).forEach(([key, openingHours]) => {
+    const dayOfWeek = key as keyof Payload;
+    result[dayOfWeek] = payload[dayOfWeek];
 
     if (openingHours[openingHours.length - 1].type === 'open') {
-      const nextDay = getNextDay(day);
-      if (nextDay?.length && payload[nextDay][0].type === 'close') {
-        const overlappedDay = payload[nextDay].shift() as OpeningHours;
+      const nextDay = getNextDay(dayOfWeek);
 
-        result[day].push(overlappedDay);
-      } else {
-        result[day].push({
-          type: 'close',
-          value: notAssigned,
-        });
-      }
+      // mutate next day's payload and get current day's close hours shifted
+      const shiftedHours = payload[nextDay].shift() as OpeningHours;
+      result[dayOfWeek].push(shiftedHours);
     }
   });
 
@@ -79,29 +57,22 @@ const formatHours = (openingHours: OpeningHours[]): Result[keyof Result] => {
     return null;
   }
 
-  const resultHours = [] as TransformedValue[];
-
-  while (openingHours.length) {
-    const { value } = openingHours.shift() as OpeningHours;
+  return openingHours.map(({ value }) => {
     const isBeforeNoon = beforeNoon(value);
-
-    const item = {
+    const item: TransformedValue = {
       hour: secondsToHours(value) % 12,
       beforeNoon: isBeforeNoon,
-    } as TransformedValue;
-
-    resultHours.push(item);
-  }
-
-  return resultHours;
+    };
+    return item;
+  });
 };
 
 export default function tranformHours(payload: Payload): Result {
-  const result: Result = {};
+  const result = {} as Result;
 
-  const reArrangedPayload = arrangeOverlappingDays(payload);
+  const shiftedPayload = shiftNightHours(payload);
 
-  Object.entries(reArrangedPayload).forEach(([dayOfWeek, todayHours]) => {
+  Object.entries(shiftedPayload).forEach(([dayOfWeek, todayHours]) => {
     const day = dayOfWeek as keyof Payload;
 
     result[day] = formatHours(todayHours);
